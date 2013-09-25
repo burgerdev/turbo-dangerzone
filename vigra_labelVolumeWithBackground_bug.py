e425c24119f8ab95a669c9c475d36f92ce517a01
Markus Döring <webmaster@burgerdev.de>
2013-09-25 12:45:25 +0200

    a vigra bug

---
+++ vigra_labelVolumeWithBackground_bug.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2
+
+import numpy as np
+import vigra
+
+def failing_vigra_test():
+	print(vigra.__version__.version)
+	v = np.zeros((10,10,10), dtype=np.uint8)
+	v[3:6,3:6,1:3] = 1
+	v[8:10,8:10,6:8] = 1
+	w = vigra.analysis.labelVolumeWithBackground(v)
+
+	assert w[3,3,1]>0, "Should be labeled"
+	assert w[8,8,6]>0, "Should also be labeled"
+
+
+if __name__ == "__main__":
+	failing_vigra_test()
