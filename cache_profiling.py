4c24b0f7ae737f2440186685fadbd576d70fe6cb
Markus Döring <webmaster@burgerdev.de>
2014-01-28 16:41:38 +0100

    codejam python profiling, thx @zaytsev

---
+++ cache_profiling.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import numpy as np
+import vigra
+
+from lazyflow.operators import OpCompressedCache, OpArrayCache, OpArrayPiper
+from lazyflow.graph import Graph
+
+from pympler.asizeof import asizeof
+from pympler.classtracker import ClassTracker
+
+if __name__ == "__main__":
+
+    g = Graph()
+    vol = np.random.randint(255, size=(1000,1000,10)).astype(np.uint8)
+    vol = vigra.taggedView(vol, axistags='xyz')
+    
+    pipe = OpArrayPiper(graph=g)
+    pipe.Input.setValue(vol)
+    
+    cache1 = OpArrayCache(graph=g)
+    cache1.Input.connect(pipe.Output)
+    
+    cache2 = OpCompressedCache(graph=g)
+    cache2.Input.connect(cache1.Output)
+    cache2.BlockShape.setValue((100,100,1))
+    
+    tracker = ClassTracker()
+    tracker.track_object(cache1)
+    tracker.track_object(cache2)
+    #tracker.track_class(Employee)
+    tracker.create_snapshot()
+    
+    
+    print("Size of graph (before): {}".format(asizeof(g)))
+    print("Size of array piper (before): {}".format(asizeof(pipe)))
+    print("Size of uncompressed cache (before): {}".format(asizeof(cache1)))
+    print("Size of compressed cache (before): {}".format(asizeof(cache2)))
+    
+    vol2 = cache2.Output[...].wait()
+    
+    tracker.create_snapshot()
+    
+    print("Size of graph (after): {}".format(asizeof(g)))
+    print("Size of array piper (after): {}".format(asizeof(pipe)))
+    print("Size of uncompressed cache (after): {}".format(asizeof(cache1)))
+    print("Size of compressed cache (after): {}".format(asizeof(cache2)))
+    
+    print("")
+    
+    tracker.stats.print_summary()
+
